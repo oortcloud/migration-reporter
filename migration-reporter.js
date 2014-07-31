977d860ddf900c8acfb70860736991f879a078ea
Tom Coleman <tom@thesnail.org>
2014-07-31 16:18:03 +1000

    More errors, consistent reporting

--- migration-reporter.js
+++ migration-reporter.js
@@ -1,8 +1,37 @@
 Versions = new Meteor.Collection('versions')
 
 var KNOWN_ERROR_REGEXPS = {
+  alreadyRegistered: [ /Package already registered/ ],
   awaitingDep: [ /No troposphere version of/ ],
-  gitFailed: [ /is this a git repository/, /remote: Repository not found/ ]
+  gitFailed: [ 
+    /is this a git repository/, 
+    /remote: Repository not found/, 
+    /Could not find remote branch/,
+    /The requested URL returned error/
+  ],
+  nonStandardPackageLibDir: [
+    /No such file or directory - source\/smart.json/
+  ],
+  dependencyGone: [
+    /couldn't find version/
+  ],
+  nonAtmosphereDeps: [
+    /Can't migrate packages with non atmosphere dependencies/
+  ],
+  depNotInSmartJson: [
+    /Cannot find anything about package/,
+    // weak dependency
+    /You must specify a version constraint for the following packages/
+  ],
+  badNpmDep: [
+    /couldn't install npm package/
+  ],
+  buildError: [
+    /While building the package/
+  ],
+  nonSemverVersion: [
+    /The package version \(specified with Package.describe\) must be valid semver/
+  ]
 }
 
 var errorMatches = function(regexps) {
@@ -18,7 +47,8 @@ if (Meteor.isClient) {
   Meteor.subscribe('errorVersions');
   
   UI.registerHelper('count', function(name) {
-    return Counts.get(name);
+    console.log(name.toString())
+    return Counts.get(name.toString());
   });
   
   UI.registerHelper('truncateLines', function(message) {
@@ -32,6 +62,8 @@ if (Meteor.isClient) {
     versions: function() {
       return Versions.find({error: {$exists: true}});
     },
+    
+    knownErrorTypes: _.keys(KNOWN_ERROR_REGEXPS),
     
     expand: function() {
       return Session.equals('expanded', EJSON.stringify(_.pick(this, 'name', 'version')));
@@ -49,16 +81,13 @@ if (Meteor.isServer) {
   Versions._ensureIndex({name: 1, version: 1}, {unique: true});
   
   Meteor.publish('counts', function() {
+    var self = this;
     publishCount(this, 'allVersions', Versions.find({}, {fields: {_id: true}}));
     publishCount(this, 'completeVersions', Versions.find({complete: true}, {fields: {_id: true}}));
-    
-    publishCount(this, 'errorVersions', Versions.find({error: {$exists: true}}, {fields: {_id: true}}));
-    publishCount(this, 'awaitingDepVersions', 
-      Versions.find(errorMatches(KNOWN_ERROR_REGEXPS.awaitingDep), {fields: {_id: true}})
-    );
-    publishCount(this, 'gitFailedVersions', 
-      Versions.find(errorMatches(KNOWN_ERROR_REGEXPS.gitFailed), {fields: {_id: true}})
-    );
+    _.each(KNOWN_ERROR_REGEXPS, function(regexps, name) {
+      publishCount(self, name, 
+        Versions.find(errorMatches(regexps)), {fields: {_id: true}});
+    })
   });
   
   Meteor.publish('errorVersions', function() {
